62268ce4492b7374c12150e3632c5302390386de
jayxgreen <green4jn@mail.uc.edu>
2024-09-05 12:05:58 -0400

    ProductWriter.java

--- Practicum01/src/ProductWriter.java
+++ Practicum01/src/ProductWriter.java
@@ -9,66 +9,49 @@ import static java.nio.file.StandardOpenOption.CREATE;
 
 public class ProductWriter {
     public static void main(String[] args) {
-        ArrayList<String> folks = new ArrayList<>();
+        ArrayList<String> products = new ArrayList<>();
         Scanner in = new Scanner(System.in);
         boolean doneInput = false;
-        String ID = "";
-        String firstName = "";
-        String lastName = "";
-        String title = "";
-        String personRec = "";
+        String ID;
+        String name;
+        String description;
+        double cost;
 
-        int YOB = 0;
+        do {
+            ID = SafeInput.getRegExString(in, "Enter the ID [6 digits]", "\\d{6}");
+            name = SafeInput.getNonZeroLenString(in, "Enter the product name");
+            description = SafeInput.getNonZeroLenString(in, "Enter the product description");
 
-        // uses a fixed known path:
-        //  Path file = Paths.get("c:\\My Documents\\data.txt");
+            // Cast the result to double, assuming getRangedDouble is actually returning an int
+            cost = (double) SafeInput.getRangedDouble(in, "Enter the product cost", 1, 9999);
 
-        // use the toolkit to get the current working directory of the IDE
-        // will create the file within the Netbeans project src folder
-        // (may need to adjust for other IDE)
-        // Not sure if the toolkit is thread safe...
+            String productRec = String.format("%s, %s, %s, %.2f", ID, name, description, cost);
+            products.add(productRec);
+
+            doneInput = SafeInput.getYNConfirm(in, "Are you done entering products?");
+
+        } while(!doneInput);
+
+        // Prompt for file name
+        String fileName = SafeInput.getNonZeroLenString(in, "Enter the name for the output text file (without extension)");
+
         File workingDirectory = new File(System.getProperty("user.dir"));
-        Path file = Paths.get(workingDirectory.getPath() + "\\src\\PersonData.txt");
-
-        do
-        {
-            ID = SafeInput.getNonZeroLenString(in, "Enter the ID [6 digits]: ");
-            firstName = SafeInput.getNonZeroLenString(in, "Enter the first name: ");
-            lastName = SafeInput.getNonZeroLenString(in, "Enter the last name:");
-            title = SafeInput.getNonZeroLenString(in, "Enter the title:");
-            YOB = SafeInput.getRangedInt(in, "Enter the year of birth:", 1000, 9999);
-
-            personRec = ID + ", " + firstName + ", " + lastName + ", " + title + ", " + YOB;
-            folks.add(personRec);
-
-            doneInput = SafeInput.getYNConfirm(in, "Are you done?");
-
-        }while(!doneInput);
-
+        Path file = Paths.get(workingDirectory.getPath() + File.separator + fileName + ".txt");
 
         try {
-            // Typical java pattern of inherited classes
-            // we wrap a BufferedWriter around a lower level BufferedOutputStream
             OutputStream out =
                     new BufferedOutputStream(Files.newOutputStream(file, CREATE));
             BufferedWriter writer =
                     new BufferedWriter(new OutputStreamWriter(out));
 
-            // Finally can write the file LOL!
-
-            String[] recs = new String[0];
-            for (String rec : recs) {
-                writer.write(rec, 0, rec.length());  // stupid syntax for write rec
-                // 0 is where to start (1st char) the write
-                // rec. length() is how many chars to write (all)
-                writer.newLine();  // adds the new line
-
+            for (String rec : products) {
+                writer.write(rec, 0, rec.length());
+                writer.newLine();
             }
-            writer.close(); // must close the file to seal it and flush buffer
-            System.out.println("Data file written!");
+            writer.close();
+            System.out.println("Data file written to: " + file.toString());
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 }
