cb60cd82ec4fab7356a101b04096ffc3f2bcb7a3
jayxgreen <green4jn@mail.uc.edu>
2024-09-23 00:30:27 -0400

    Add files via upload

---
+++ Lab 03 Filter Interface/src/RecLister.java
@@ -0,0 +1,30 @@
+import java.awt.Rectangle;
+import java.util.ArrayList;
+
+public class RecLister {
+    public static void main(String[] args) {
+        ArrayList<Rectangle> rectangles = new ArrayList<>();
+
+        // Add 10 rectangles
+        rectangles.add(new Rectangle(4, 1));
+        rectangles.add(new Rectangle(5, 2));
+        rectangles.add(new Rectangle(6, 3));
+        rectangles.add(new Rectangle(3, 4));
+        rectangles.add(new Rectangle(5, 1));
+        rectangles.add(new Rectangle(6, 2));
+        rectangles.add(new Rectangle(7, 3));
+        rectangles.add(new Rectangle(6, 4));
+        rectangles.add(new Rectangle(4, 5));
+        rectangles.add(new Rectangle(1, 6));
+
+        Filter bigRectangleFilter = new RecFilter();
+
+        System.out.println("Rectangles with perimeter > 10:");
+        for (Rectangle rect : rectangles) {
+            if (bigRectangleFilter.accept(rect)) {
+                System.out.println("Rectangle: width = " + rect.width + ", height = " + rect.height +
+                        ", perimeter = " + (2 * (rect.width + rect.height)));
+            }
+        }
+    }
+}
