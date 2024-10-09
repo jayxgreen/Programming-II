57684cecf13d631cefa1edf1599f765b4fa09fa5
jayxgreen <green4jn@mail.uc.edu>
2024-10-09 19:55:08 -0400

    Add files via upload

---
+++ Lab05/src/LastUsedStrat.java
@@ -0,0 +1,22 @@
+public class LastUsedStrat implements Strategy {
+    private String lastPlayerMove;
+
+    public LastUsedStrat(String lastPlayerMove) {
+        this.lastPlayerMove = lastPlayerMove;
+    }
+
+    @Override
+    public String determineMove() {
+        return getWinningMove(lastPlayerMove);
+    }
+
+    private String getWinningMove(String move) {
+        switch (move) {
+            case "Rock": return "Paper"; // Paper beats Rock
+            case "Paper": return "Scissors"; // Scissors beats Paper
+            case "Scissors": return "Rock"; // Rock beats Scissors
+            default: return "Rock"; // Fallback, should never happen
+        }
+    }
+}
+
